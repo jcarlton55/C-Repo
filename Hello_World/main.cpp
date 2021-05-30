834615f696d879371ddb1170f7622f3b916fc254
Justin Carlton <justincarlton55@yahoo.com>
2021-05-30 09:51:41 -0400

    Commit Hello World

---
+++ Hello_World/main.cpp
@@ -0,0 +1,13 @@
+/*
+Author : Justin Carlton
+Date : 5/30/2021
+Purpose : Basic Hello World Program to test VSCode environment config
+*/
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello World!" << std::endl;
+
+    return 0;
+}
